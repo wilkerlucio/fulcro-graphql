67fcae43a66559faff4533473c81548dd14f7f63
Wilker Lucio <wilkerlucio@gmail.com>
2017-08-17 22:08:02 -0300

    Cleanup network code

--- src/com/wsscode/fulcro_graphql/network.cljs
+++ src/com/wsscode/fulcro_graphql/network.cljs
@@ -28,10 +28,10 @@
 
 (defn parse [env tx]
   (parser
-    (assoc env
-      ::p/js-key-transform js-name
-      ::p/js-value-transform sc/coerce
-      ::p/reader p/js-obj-reader)
+    (merge {::p/js-key-transform   js-name
+            ::p/js-value-transform sc/coerce
+            ::p/reader             p/js-obj-reader}
+           env)
     tx))
 
 (defn http [{::keys [url body method headers]
@@ -73,39 +73,10 @@
         (-> (query #::{:url url :q edn})
             <? ok)
         (catch :default e
-          (js/console.log "ERROR NET" e)
+          (js/console.log "Network error" e)
           (error e)))))
 
-  (start [this app]))
+  (start [_]))
 
 (defn graphql-network [url]
   (map->Network {:url url}))
-
-(comment
-  (go
-    (->> (query #::{:url "https://api.graph.cool/simple/v1/cj5k0e0j74cpv0122vmzoqzi0"
-                    :q   [{:link/all-links [:link/id :link/title :link/url :link/updated-at]}]})
-         <? js/console.log))
-
-  (go
-    (->> (query #::{:url "https://api.github.com/graphql?access_token="
-                    :q   `[{:viewer
-                            [:login
-                             ({:repositories
-                               [{:nodes [:name :description]}]}
-                               {:last 10})]}
-                           ({:repository [:id :description]}
-                             {:name "spec-coerce" :owner "wilkerlucio"})]})
-         <? js/console.log))
-
-  (println (gql/query->graphql [{:link/all-links [:link/id :link/title :link/url :link/updated-at]}]
-                               {::gql/js-name js-name}))
-
-  (js/console.log
-    (gql/query->graphql `[{:viewer
-                           [:login
-                            ({:repositories
-                              [{:nodes [:name :description]}]}
-                              {:last 10})]}
-                          ({:repository [:id :description]}
-                            {:name "spec-coerce" :owner "wilkerlucio"})])))
